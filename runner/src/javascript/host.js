06fd4ff09c2b5f259589bb1b12ef15ee62e9be90
Philip Weaver <simplygreatwork@gmail.com>
2019-10-25 01:25:44 -0500

    shove

--- runner/src/javascript/host.js
+++ runner/src/javascript/host.js
@@ -18,6 +18,14 @@ function Host() {
 			value: 'i32',
 			mutable: true
 		}, 0),
+		type_strings: new WebAssembly.Global({
+			value: 'i32',
+			mutable: true
+		}, 0),
+		on_memory_allocate_block: new WebAssembly.Global({
+			value: 'i32',
+			mutable: true
+		}, 0),
 		print_string: function(offset) {
 			var array = new Uint8Array(this.memory.buffer)
 			let type = array[offset]
